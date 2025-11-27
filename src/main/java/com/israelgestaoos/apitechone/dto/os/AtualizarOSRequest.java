9b683fb5dd338d61f9f80d1bd01546de682545f5
Israel Jesus <wlisrael6@gmail.com>
2025-11-26 21:56:51 -0300

    atualizado  o arquivo de'AtualizarOSRequest 'para o put de alteraçoes

--- src/main/java/com/israelgestaoos/apitechone/dto/os/AtualizarOSRequest.java
+++ src/main/java/com/israelgestaoos/apitechone/dto/os/AtualizarOSRequest.java
@@ -1,12 +1,18 @@
 package com.israelgestaoos.apitechone.dto.os;
 
+import com.israelgestaoos.apitechone.model.Usuario;
 import com.israelgestaoos.apitechone.model.enums.Prioridade;
 import com.israelgestaoos.apitechone.model.enums.StatusOS;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class AtualizarOSRequest {
 
+    private LocalDateTime dataAtualizacao;
+    private Usuario tecnico;
+    private String titulo;
     private String descricao;
     private Prioridade prioridade;
     private StatusOS status;
